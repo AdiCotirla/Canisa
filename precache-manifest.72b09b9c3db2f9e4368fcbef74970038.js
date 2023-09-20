485eccceeac8c0b355c1c006e0f385d5eb0739a4
AdiCotirla <adioprea53@gmail.com>
2023-09-20 15:13:42 +0300

    Updates

---
+++ precache-manifest.72b09b9c3db2f9e4368fcbef74970038.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "baf74e4711cc5d49c415",
+    "url": "/Canisa/static/css/main.4c6c215e.chunk.css"
+  },
+  {
+    "revision": "baf74e4711cc5d49c415",
+    "url": "/Canisa/static/js/main.baf74e47.chunk.js"
+  },
+  {
+    "revision": "a55ae8da76b6c0c45aec",
+    "url": "/Canisa/static/js/1.a55ae8da.chunk.js"
+  },
+  {
+    "revision": "46845d95280fe21d7e48",
+    "url": "/Canisa/static/js/2.46845d95.chunk.js"
+  },
+  {
+    "revision": "9dd33d9eb2cdaff02862",
+    "url": "/Canisa/static/js/runtime~main.9dd33d9e.js"
+  },
+  {
+    "revision": "360799e44851935909213564f92c1157",
+    "url": "/Canisa/static/media/aslan.360799e4.jpg"
+  },
+  {
+    "revision": "6cd0c787b00f766aa266d2934b922d27",
+    "url": "/Canisa/static/media/logo.6cd0c787.png"
+  },
+  {
+    "revision": "1e51cbf60d26f66803e33154ecefbd16",
+    "url": "/Canisa/static/media/rottweiler-7564116_1280.1e51cbf6.webp"
+  },
+  {
+    "revision": "068e235711d8a04883caac78b14f07d4",
+    "url": "/Canisa/static/media/paw.068e2357.png"
+  },
+  {
+    "revision": "b0d341c93d31b2056a3b24b50b5ffbf8",
+    "url": "/Canisa/static/media/679ef5c0855c5caf4f68c50d92e0d457.b0d341c9.jpg"
+  },
+  {
+    "revision": "dc9039af5402f65044720f4f7d471eda",
+    "url": "/Canisa/static/media/descend.dc9039af.jpg"
+  },
+  {
+    "revision": "ed060d0d853d1409f0f5f0d4813cf53a",
+    "url": "/Canisa/static/media/train.ed060d0d.jpg"
+  },
+  {
+    "revision": "964604fa65bd0a5ee27ecb6289f9f963",
+    "url": "/Canisa/static/media/273933819_147470744343347_8122590503467748651_n.964604fa.jpg"
+  },
+  {
+    "revision": "6fdabac9fa5f0eeaab5f4b00534778b9",
+    "url": "/Canisa/static/media/catei.6fdabac9.png"
+  },
+  {
+    "revision": "92e9fbfbe48b86058719b7880d515c42",
+    "url": "/Canisa/static/media/back.92e9fbfb.jpg"
+  },
+  {
+    "revision": "3d9327b87270ed417f60292edf653093",
+    "url": "/Canisa/static/media/catei2.3d9327b8.png"
+  },
+  {
+    "revision": "18f4a9a74815a9492634cce9b00f63a8",
+    "url": "/Canisa/static/media/map.18f4a9a7.jpg"
+  },
+  {
+    "revision": "3daca065c791060ede34a79c48ac5114",
+    "url": "/Canisa/index.html"
+  }
+];
